e40620b7342f456a0d406b8f3e4bd96c82a1f06c
Srivathsan Murali <sri@vathsan.com>
2020-03-04 10:47:27 +0100

    Add documentation for presigned put/get

--- lib/minio.ex
+++ lib/minio.ex
@@ -27,6 +27,22 @@ defmodule Minio do
   ```
   The region has to be specified. It is not automatically retreived.
   The default region is set to "us-east-1"
+
+  Once the client sturct is created, the following api functions can
+  be used.
+  
+  ### Pesigned Put Object
+  Generate a presigned url to put an object to an minio instance.
+  
+  ```elixir
+  {:ok, put_url} = Minio.presign_put_object(client, bucket_name: "test", object_name: "test3.png")
+  ```
+
+  ### Presigned Get Object
+  Generate a presigned url to get an object from an minio instance.
+  ```elixir
+  {:ok, get_url} = Minio.presign_get_object(client, bucket_name: "test", object_name: "test3.png")
+  ```
   """
 
   defstruct endpoint: nil,
@@ -43,14 +59,30 @@ defmodule Minio do
 
   alias Minio.Signer
 
+  @spec presign_put_object(t, Keyword.t()) :: {:ok, String.t()} | {:error, String.t()}
   @doc """
   Presigns a put object and provides a url
+
+  ## Options
+
+    * `:bucket_name` - The name of the minio bucket.
+    * `:object_name` - The object name or path.
+    * `:request_datetime` - The datetime of request. Defaults to `DateTime.utc_now()`
+    * `:link_expiry` - The number of seconds for link expiry. Defaults to `608_400` seconds.
   """
   def presign_put_object(%Minio{} = client, opts),
     do: Signer.presigned_url(client, :put, opts)
 
+  @spec presign_get_object(t, Keyword.t()) :: {:ok, String.t()} | {:error, String.t()}
   @doc """
   Presigns a get object and provides a url
+  
+  ## Options
+
+    * `:bucket_name` - The name of the minio bucket.
+    * `:object_name` - The object name or path.
+    * `:request_datetime` - The datetime of request. Defaults to `DateTime.utc_now()`
+    * `:link_expiry` - The number of seconds for link expiry. Defaults to `608_400` seconds.
   """
   def presign_get_object(%Minio{} = client, opts),
     do: Signer.presigned_url(client, :get, opts)
