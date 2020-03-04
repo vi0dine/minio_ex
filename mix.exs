62c99be47ed001fd180e270ad01e59c5056ae3da
Srivathsan Murali <sri@vathsan.com>
2020-03-04 11:01:13 +0100

    update mix.exs and add license

--- mix.exs
+++ mix.exs
@@ -7,20 +7,34 @@ defmodule Minio.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description(),
+      name: "minio",
+      source_url: "https://github.com/srivathsanmurali/minio_ex"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   defp deps do
     [
       {:ex_doc, "~> 0.19.1", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "A client library to use the Minio API"
+  end
+
+  defp package() do
+    [
+      name: "minio",
+      maintainers: ["Srivathsan Murali"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/srivathsanmurali/minio_ex"}
     ]
   end
 end
